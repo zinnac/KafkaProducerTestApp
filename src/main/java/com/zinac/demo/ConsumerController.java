26d51a9232d3c26b2ecea5aa986ab3c78cb23fdc
l3igwill <l3igwill1994@gmail.com>
2021-09-16 23:21:32 -0500

    Add consumer, tweak prodcuer
    update pom java version
    add consumer controller for web call
    adding methods to main for consume/produce

--- src/main/java/com/zinac/demo/ConsumerController.java
+++ src/main/java/com/zinac/demo/ConsumerController.java
@@ -1,7 +1,13 @@
 package com.zinac.demo;
 
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.kafka.clients.consumer.*;
+
 
 @RestController
 public class ConsumerController {
@@ -9,6 +15,34 @@ public class ConsumerController {
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+	
+	@GetMapping("/consume")
+	public void consumeMessages() {
+		/*try {
+			while (true) {
+				ConsumerRecords<String,String> records = createConsumer().poll(Duration.ofMillis(100));
+			}
+		} catch(Exception e) {
+			return String.valueOf(e);
+		}*/
+		
+	}
+	
+	private static Consumer<String, String> createConsumer(){
+
+		Properties config = new Properties();
+		final String topic = "javatestapplication";
+		//config.put("client.id", InetAddress.getLocalHost().getHostName());
+		config.put("bootstrap.servers", "10.0.0.200:9092");
+		config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		config.put("key.deserializer", "org.apache.kafka.common.serialization.StringSerializer");
+		config.put("value.deserializer", "org.apache.kafka.common.serialization.StringSerializer");
+		
+		Consumer<String,String> consumer = new KafkaConsumer<>(config);
+		consumer.subscribe(Collections.singletonList(topic));
+		return consumer;
 	}
 
 }
--- src/main/java/com/zinac/demo/KafkaProducerTestAppApplication.java
+++ src/main/java/com/zinac/demo/KafkaProducerTestAppApplication.java
@@ -2,7 +2,13 @@ package com.zinac.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.*;
+
+import java.time.Duration;
 import java.util.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
@@ -13,8 +19,20 @@ public class KafkaProducerTestAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaProducerTestAppApplication.class, args);
-		produceMessages();
+		
+		new Thread(new Runnable(){
+			public void run() {
+				produceMessages();
+			}
+		}).start();
+		
+		new Thread(new Runnable(){
+			public void run() {
+				consumeMessages();
+			}
+		}).start();
 	}
+
 	
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
@@ -38,21 +56,19 @@ public class KafkaProducerTestAppApplication {
 		config.put("bootstrap.servers", "10.0.0.200:9092");
 		config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		config.put("key.deserializer", "org.apache.kafka.common.serialization.StringSerializer");
-		config.put("value.deserializer", "org.apache.kafka.common.serialization.StringSerializer");
 		
 		Producer<String, String> producer = new KafkaProducer<>(config);
 		int i = 0;
 
 		while(true){
 			try{
-				Thread.sleep(5000);
-				
+				Thread.sleep(500);
+				final String output = String.valueOf(i);
 				producer.send(
 				new ProducerRecord<String,String>(topic, "Test: "+i),
 				(event, ex) ->{
 					if(ex != null) ex.printStackTrace(); 
-					else System.out.printf("Produced event to topic %s",topic);
+					else System.out.printf("\nProduced event to topic %s: Test: %s",topic,output);
 				});
 			i++;
 			}
@@ -63,6 +79,42 @@ public class KafkaProducerTestAppApplication {
 				System.out.println("Program has ended."+"\n");
 			}
 		}
+	}
+	
+	public static void consumeMessages() {
+		Consumer<String,String> consumer = createConsumer();
+		while(true) {
+			try {
+			Thread.sleep(10000);
+			consumer.poll(Duration.ofMillis(1000)).forEach(record -> processRecord(record));	
+			consumer.commitSync();	
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+		
+		
+	public static void processRecord(ConsumerRecord<String,String> record) {
+		System.out.printf("\nConsumed event from topic: key:%s value:%s",record.key(),record.value());
+	}
+	
+	private static Consumer<String, String> createConsumer(){
+
+		Properties config = new Properties();
+		final String topic = "javatestapplication";
+		//config.put("client.id", InetAddress.getLocalHost().getHostName());
+		config.put("group.id", "java-apps");
+		config.put("bootstrap.servers", "10.0.0.200:9092");
+		config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		config.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		config.put("max.poll.records", "5");
+		config.put("session.timeout.ms", "30000");
+		
+		Consumer<String,String> consumer = new KafkaConsumer<>(config);
+		consumer.subscribe(Collections.singletonList(topic));
+		return consumer;
 	}
 
 }
